4367cac6b79c73f523c839f3714a13f8a15821ff
Ewald Mayr <ewald.mayr@gemuese-mayr.at>
2025-11-12 15:33:52 +0100

    Zeilenumbruch im API_URL entfernt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL =
-import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
 // ==== Typen ====
 
 type Farmer = {
